5cee1bea9a10e1fdf60a794da715ca01160a529b
Derek Ball <derekball.school@gmail.com>
2015-06-26 11:38:55 -0600

    both api calls working, with images

--- routes/index.js
+++ routes/index.js
@@ -11,14 +11,26 @@ router.get('/', function(req, res, next) {
   .header("X-Mashape-Key", "kJOUgKpJFamshTFuvY7O2GnTccWup1ilLFPjsnmEQTvRxk1qPG")
   .header("Accept", "text/plain")
   .end(function (result) {
-    console.log(
-      result.status,
-      result.headers,
-      result.body);
+    // console.log(
+    //   result.status,
+    //   result.headers,
+    //   result.body);
 
-  res.render('index', { title: "Let's get Trailblazin'", result: result.body.places });
+  unirest
+  .get("https://twinesocial.p.mashape.com/v1/content?campaign=louboutin")
+  .header("X-Mashape-Key", "fn4a5vCfS4mshcgPHf87Ksd6rjhKp1MBa0YjsnDWurXz4ZsZsW")
+  .header("Accept", "application/json")
+  .end(function (result2) {
+    // console.log(result2.status, result2.headers, result2.body);
+    console.log(result2.body);
+
+  res.render('index', { title: "Let's get Trailblazin'", result: result.body.places, social: result2.body.rows });
 })
-
+})
 });
+
+
+
+
 
 module.exports = router;
